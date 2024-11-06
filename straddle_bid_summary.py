162105805926cbd59cfd43d5060835a61d26a1a0
VipulAnand <vipulredhat@gmail.com>
2024-11-06 11:48:19 +0530

    add: dte, expiry, number of strikes, decay of each strike, discinct strikes which gave decay, change: decay each strike = max decay each strike - min decay each strike

--- straddle_bid_summary.py
+++ straddle_bid_summary.py
@@ -10,6 +10,9 @@ from matplotlib.widgets import Cursor
 import numpy as np
 from common import data_path, b_days
 
+pd.set_option('display.max_columns', None)
+# pd.set_option("display.max_rows", None)
+
 df_cummulative = pd.DataFrame(columns=['Date'])
 df_bids=pd.DataFrame(columns=['Date','Straddle_Name','bid','bid_time','bid_price','decay','decay_time','decay_price'])
 start_date = str(input('Enter start date in YYYY-MM-DD format:\t'))
@@ -20,9 +23,32 @@ if start_date > end_date:
     raise ValueError('Start date must be less than end date')
 use_date_range = [use_date.date() for use_date in b_days if start_date<=use_date<=end_date]
 
+def calc_business_days(chk_date, exp_date):
+    holidays_23 = ['2023-01-26', '2023-03-07', '2023-03-30', '2023-04-04', '2023-04-07', '2023-04-14', '2023-05-01',
+                   '2023-06-29', '2023-08-15', '2023-09-19', '2023-10-02', '2023-10-24', '2023-11-14', '2023-11-27',
+                   '2023-12-25']
+    holidays_24 = ['2024-01-22', '2024-01-26', '2024-03-08', '2024-03-25', '2024-03-29', '2024-04-11', '2024-04-17',
+                   '2024-05-01', '2024-05-20', '2024-06-17', '2024-07-17', '2024-08-15', '2024-10-02', '2024-11-01',
+                   '2024-11-15', '2024-12-25']
+    holidays = holidays_23 + holidays_24
+    holidays = pd.to_datetime(holidays)  # Convert string dates to datetime objects
+    excluded_dates = ['2024-01-20', '2024-03-02', '2024-05-04', '2024-05-18', '2024-06-01', '2024-07-06', '2024-08-03',
+                      '2024-09-14', '2024-10-05', '2024-11-09', '2024-12-07']
+    excluded_dates = pd.to_datetime(excluded_dates)
+    holidays = holidays[~holidays.isin(excluded_dates)]  # Remove the excluded dates from the holidays list
+
+    exp_date = pd.to_datetime(exp_date)
+    chk_date = pd.to_datetime(chk_date)
+
+    business_days_left = pd.bdate_range(start=chk_date, end=exp_date, holidays=holidays, freq='C', weekmask='1111100')
+    actual_bus_days = len(business_days_left) - 1
+    return actual_bus_days
+
 def bid_summary(df):
     use_date = df.get('Date').unique()[0]
     straddle_name = df.get('straddle_name').unique()[0]
+    expiry_date = df.get('expiry').unique()[0]
+    dte = calc_business_days(pd.to_datetime(use_date), pd.to_datetime(expiry_date))
     dfbid = df.copy()
     dfbid['cummax']=dfbid['Straddle'].cummax()
     dfbid['cummin']=dfbid['Straddle'].cummin()
@@ -34,10 +60,11 @@ def bid_summary(df):
     bid_price=dfbid.loc[dfbid['Bid']==bid].reset_index()['Straddle']
     decay_price=dfbid.loc[dfbid['Decay']==decay].reset_index()['Straddle']
     decay_time=dfbid.loc[dfbid['Decay']==decay].reset_index()['Timestamp']
-    each_row = pd.DataFrame([[use_date,straddle_name,bid,bid_time[0],bid_price[0],decay,decay_time[0],decay_price[0]]],
-                columns=['Date','Straddle_Name','bid','bid_time','bid_price','decay','decay_time','decay_price'])
+    each_row = pd.DataFrame([[use_date,straddle_name, expiry_date, dte, bid, bid_time[0],bid_price[0],decay,decay_time[0],decay_price[0]]],
+                columns=['Date','Straddle_Name', 'expiry', 'dte', 'bid','bid_time','bid_price','decay','decay_time','decay_price'])
     # df_cummulative = pd.concat([df_cummulative, dfbid])
     dfbid = dfbid.drop(['cummax', 'cummin'], axis=1)
+    dfbid['dte'] = dte
     if bid_time[0] < decay_time[0]:
         start_time = bid_time[0]
         end_time = decay_time[0]
@@ -45,7 +72,6 @@ def bid_summary(df):
         start_time = decay_time[0]
         end_time = bid_time[0]
     dfbid = dfbid.query('Timestamp >= @start_time and Timestamp <= @end_time')
-
     dfbid.sort_values(by=['Decay'], ascending = False, inplace=True)
     return each_row, dfbid
 
@@ -100,12 +126,39 @@ def calc_bid_and_cummulative():
             num_strikes = len(df_cummulative.query("date == @each_bid_date and straddle_name == @each_strd_name"))
             df_bids.loc[(df_bids['date'] == each_bid_date) & (df_bids['straddle_name'] == each_strd_name), 'num_strikes'] = num_strikes
             # ---------------------------------------------------------------------------------------
-            st_list = df_cummulative.query("date == @each_bid_date and straddle_name == @each_strd_name")['strike'].unique().tolist()
-            st_num = len(st_list)
-            st_str = ','.join(map(str, sorted(st_list)))
+            strike_list = df_cummulative.query("date == @each_bid_date and straddle_name == @each_strd_name")['strike'].unique().tolist()
+            strike_decay_list = []
+            for each_strike in strike_list:
+                max_decay = df_cummulative.query(
+                    "date == @each_bid_date and straddle_name == @each_strd_name and strike == @each_strike"
+                )['decay'].max()
+                min_decay = df_cummulative.query(
+                    "date == @each_bid_date and straddle_name == @each_strd_name and strike == @each_strike"
+                )['decay'].min()
+
+                # if max_decay == min_decay == 0:
+                #     continue
+                if max_decay == min_decay:
+                    final_decay = max_decay
+                else:
+                    final_decay = max_decay - min_decay
+                strike_decay_list.append(round((final_decay),2))
+            st_num = len(strike_list)
+            st_str = ','.join(map(str, sorted(strike_list)))
+            decay_str = ','.join(map(str, strike_decay_list))
+            df_bids.loc[(df_bids['date'] == each_bid_date) & (df_bids['straddle_name'] == each_strd_name),'decay'] = sum(strike_decay_list)
             df_bids.loc[(df_bids['date'] == each_bid_date) & (df_bids['straddle_name'] == each_strd_name), 'num_strikes'] = int(st_num)
             df_bids.loc[(df_bids['date'] == each_bid_date) & (df_bids['straddle_name'] == each_strd_name), 'distinct_strikes'] = st_str
+            df_bids.loc[(df_bids['date'] == each_bid_date) & (df_bids['straddle_name'] == each_strd_name), 'strike_decay'] = decay_str
     df_bids['num_strikes'] = df_bids['num_strikes'].astype(int)
+    # df_bids['decay'] = df_bids['bid_price'] - df_bids['decay_price']
+    df_cummulative['dte'] = df_cummulative['dte'].apply(lambda row: int(row))
+    df_bids['dte'] = df_bids['dte'].apply(lambda row: int(row))
+    # df_bids = df_bids[['date', 'straddle_name', 'expiry', 'dte', 'bid', 'bid_time', 'decay', 'decay_time', 'num_strikes', 'distinct_strikes', 'strike_decay']]
+    # df_cummulative = df_cummulative[
+    #     ['date', 'timestamp', 'straddle_name', 'expiry', 'dte', 'strike', 'straddle', 'bid', 'decay']]
+    df_cummulative = df_cummulative.query('straddle_name not in ["NIFTY_NM", "NIFTY_NW", "NIFTY_CM", "BANKNIFTY_NW", "FINNIFTY_NW", "MIDCPNIFTY_NW"]')
+    df_bids = df_bids.query('straddle_name not in ["NIFTY_NM", "NIFTY_NW", "NIFTY_CM", "BANKNIFTY_NW", "FINNIFTY_NW", "MIDCPNIFTY_NW"]')
     return df_bids, df_cummulative
 
 
@@ -114,65 +167,7 @@ def plot_graph(use_date, strd_name):
     # strd_name = str(input('Enter straddle_name(BANKNIFTY_CW, BANKNIFTY_NW etc) for which graph needs to be plotted:\t'))
     dfx = df_cummulative.query('date == @use_date and straddle_name == @strd_name')
     dfx['date_timestamp'] = pd.to_datetime(dfx['date'] + ' ' + dfx['timestamp'])
-    # # ----------------------------------------------------------------
-    # # ax = plt.gca()
-    # # dfx.plot(kind = 'line', x = 'date_timestamp', y = 'strike', color = 'red', ax = ax)
-    # # dfx.plot(kind = 'line', x = 'date_timestamp', y = 'decay', color = 'blue', ax = ax)
-    # # ----------------------------------------------------------------
-    # fig, ax = plt.subplots()
-    # ax1 = ax.twinx()
-    # ax.plot(dfx.date_timestamp, dfx.strike, color = 'red')
-    # ax1.plot(dfx.date_timestamp, dfx.decay, color = 'blue')
-    # ax.set_xlabel('Timestamp', fontsize = 8)
-    # ax.set_ylabel('Strike', fontsize = 8)
-    # ax1.set_ylabel('Decay', fontsize = 8)
-    # # ----------------------------------------------------------------
-    # plt.title('Decay vs Strike')
-    # ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M%S'))
-    # plt.tight_layout()
-    # plt.xticks(rotation=90, fontsize=8)
-    # # cursor = Cursor(ax, useblit=True, color='green', linewidth=1)
-    # #
-    # # def format_tooltip(event):
-    # #     if event.inaxes == ax:
-    # #         # Get nearest x-value (timestamp)
-    # #         x_value = mdates.num2date(event.xdata).strftime('%Y-%m-%d %H:%M:%S')
-    # #         # Get nearest y-values (strike and decay)
-    # #         strike_value = dfx.loc[(dfx['date_timestamp'] - pd.Timestamp(event.xdata)).abs().idxmin(), 'strike']
-    # #         decay_value = dfx.loc[(dfx['date_timestamp'] - pd.Timestamp(event.xdata)).abs().idxmin(), 'decay']
-    # #         print(f"Timestamp: {x_value}, Strike: {strike_value}, Decay: {decay_value}")
-    # #
-    # # fig.canvas.mpl_connect('motion_notify_event', format_tooltip)
-    #
-    # cursor = Cursor(ax, useblit=True, color='green', linewidth=1)
-    #
-    # # Create annotation for tooltip
-    # annot = ax.annotate("", xy=(0, 0), xytext=(20, 20), textcoords="offset points",
-    #                     bbox=dict(boxstyle="round", fc="w"),
-    #                     arrowprops=dict(arrowstyle="->"))
-    # annot.set_visible(False)
-    #
-    # # Tooltip formatter
-    # def format_tooltip(event):
-    #     if event.inaxes == ax:
-    #         x_value = mdates.num2date(event.xdata).strftime('%Y-%m-%d %H:%M:%S')
-    #
-    #         # Find the nearest data points for strike and decay
-    #         nearest_index = (dfx['date_timestamp'] - pd.Timestamp(mdates.num2date(event.xdata))).abs().idxmin()
-    #         strike_value = dfx.loc[nearest_index, 'strike']
-    #         decay_value = dfx.loc[nearest_index, 'decay']
-    #
-    #         # Update annotation text
-    #         annot.xy = (event.xdata, event.ydata)
-    #         text = f"Timestamp: {x_value}\nStrike: {strike_value}\nDecay: {decay_value}"
-    #         annot.set_text(text)
-    #         annot.set_visible(True)
-    #         fig.canvas.draw_idle()
-    #
-    # # Connect the format_tooltip function to the mouse motion event
-    # fig.canvas.mpl_connect('motion_notify_event', format_tooltip)
-    # dfx.set_index('strike', inplace=True)
-    df_grouped = dfx.groupby('strike')['decay'].max().reset_index()
+    df_grouped = dfx.groupby('strike')['decay'].sum().reset_index()
     ax = df_grouped.plot(kind = 'bar', x = 'strike', y = 'decay', legend = True, grid = True)
     ax.set_xlabel('Strike')
     ax.set_ylabel('Decay')
@@ -185,18 +180,16 @@ def plot_graph(use_date, strd_name):
 #     df_bids, df_cummulative = calc_bid_and_cummulative(use_date_range)
 
 df_bids, df_cummulative = calc_bid_and_cummulative()
-pd.set_option('display.max_columns', None)
-# pd.set_option("display.max_rows", None)
-
+print(f'df_bids is \n{df_bids}')
+print(f'df_cummulative is \n{df_cummulative}')
 
 # function to plot the curve -
 # query the required dates from df_cummulative and plot the time vs decay curve
-
-print(f'df_bids is \n{df_bids}')
-print(f'df_cummulative is \n{df_cummulative}')
 
 strd_name = str(input('Enter straddle_name(BANKNIFTY_CW, BANKNIFTY_NW etc) for which graph needs to be plotted:\t'))
 print(f'your choice for dates - {df_cummulative["date"].unique().tolist()}')
 use_date = str(input('Enter date in format - YYYY-MM-DD\t'))
 # use_date = pd.to_datetime(use_date)
 plot_graph(use_date, strd_name)
+
+
